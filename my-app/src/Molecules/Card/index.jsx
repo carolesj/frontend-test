c79756e862f0c049d965d6b3e220b5b43998660f
carolesj <carolinejesuino.ns@gmail.com>
2020-12-14 23:33:52 -0300

    feat: generalize HeroCard to become Card
    Refactor code making the card generic. Add styles to able it to be used as a comic card.
    
    "closes #7" "closes #12"

---
+++ my-app/src/Molecules/Card/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import CardInfo from 'Atoms/CardInfo';
+import CardPicture from 'Atoms/CardPicture';
+import './Card.scss';
+
+const Card = ({isHero, comicName, heroName, isFavorite, onHeartClick, onHeroNameClick}) => (
+    <div className='Card'>
+        <CardPicture isHero={isHero}/>
+        <CardInfo isHero={isHero} comicName={comicName} heroName={heroName} isFavorite={isFavorite} onHeartClick={()=>{}} onHeroNameClick={()=>{}}/>
+    </div>
+);
+
+export default Card;
+
+CardInfo.propTypes = {
+    isHero: PropTypes.bool,
+    heroName: PropTypes.string,
+    isFavorite: PropTypes.bool,
+    onHeartClick: PropTypes.func.isRequired,
+    onHeroNameClick: PropTypes.func.isRequired
+};
+
+CardInfo.defaultProps = {
+    isHero: true,
+    heroName: '',
+    isFavorite: false
+};
