4e9d51ba496cc28d7fa2122efd70927970bfdc33
carolesj <carolinejesuino.ns@gmail.com>
2020-12-14 21:42:17 -0300

    feat: generalize page header
    Refactor searchbar and about page to be generic and extend the logic to hero page.
    
    "closes #5", "closes #3"

--- my-app/src/Atoms/AboutPage/index.jsx
+++ my-app/src/Atoms/AboutPage/index.jsx
@@ -1,13 +1,31 @@
 import React from 'react';
-import {ReactComponent as Logo} from 'Assets/Logo/Logo.svg'
-import './AboutPage.scss'
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import {ReactComponent as Logo} from 'Assets/Logo/Logo.svg';
+import LogoPng from 'Assets/Logo/Group.png';
+import './AboutPage.scss';
 
-const AboutPage = () => (
-    <div className='AboutPage'>
-        <Logo />
-        <h3>EXPLORE O UNIVERSO</h3>
-        <span>Mergulhe no domínio de todos os personagens clássicos que você ama - e aqueles que você descobrirá em breve!</span>
+const AboutPage = ({isHeroPage}) => (
+    <div className={classNames('AboutPage', {'AboutPage--hero-page': isHeroPage})}>
+        {isHeroPage ? <img src={LogoPng} alt='Logo Pequena' style={{width: '50%'}} />: <Logo />}
+        {!isHeroPage && (
+            <>
+                <h3>EXPLORE O UNIVERSO</h3>
+                <span>
+                    Mergulhe no domínio de todos os personagens clássicos que você ama - 
+                    e aqueles que você descobrirá em breve!
+                </span>
+            </>
+        )}
     </div>
 );
 
 export default AboutPage;
+
+AboutPage.propTypes = {
+    isHeroPage: PropTypes.bool
+};
+
+AboutPage.defaultProps = {
+    isHeroPage: false
+};
--- my-app/src/Molecules/PageHeader/index.jsx
+++ my-app/src/Molecules/PageHeader/index.jsx
@@ -1,13 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import AboutPage from 'Atoms/AboutPage';
 import SearchBar from 'Atoms/SearchBar';
 import './PageHeader.scss';
 
-const PageHeader = () => (
-    <div className='PageHeader'>
-        <AboutPage />
-        <SearchBar />
+const PageHeader = ({isHeroPage}) => (
+    <div
+        className={classNames('PageHeader', {'PageHeader--hero-page': isHeroPage})}
+    >
+        <AboutPage isHeroPage={isHeroPage} />
+        <SearchBar isHeroPage={isHeroPage} />
     </div>
 );
 
 export default PageHeader;
+
+PageHeader.propTypes = {
+    isHeroPage: PropTypes.bool
+};
+
+PageHeader.defaultProps = {
+    isHeroPage: false
+};
