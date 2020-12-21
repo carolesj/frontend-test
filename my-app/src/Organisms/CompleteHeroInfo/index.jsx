cc0f251183b56c4f9e22e9ac9c868b215c8122c3
carolesj <carolinejesuino.ns@gmail.com>
2020-12-21 02:41:06 -0300

    refactor: clean unused import

--- my-app/src/Organisms/CompleteHeroInfo/index.jsx
+++ my-app/src/Organisms/CompleteHeroInfo/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import classNames from 'classnames';
 import {ReactComponent as HeartIcon} from 'Assets/Icons/Heart.svg';
 import HeroStats from 'Molecules/HeroStats';
 import './CompleteHeroInfo.scss';
